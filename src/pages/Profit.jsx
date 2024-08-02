2c011b0d38172e4f52219a92f759519fca7163b5
yooosef khedri <yooosef.khedri@gmail.com>
2024-08-02 13:36:15 +0330

    update modal reusability for thin modals

--- src/pages/Profit.jsx
+++ src/pages/Profit.jsx
@@ -1,25 +1,15 @@
 import Header from "../components/Header";
 import ProfitView from "../components/profit/ProfitView";
 import Tabs from "../components/profit/Tabs";
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: #000000;
-  width: 450px;
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  padding: 15px 20px;
-`;
+import ThinModal from "../components/ThinModal";
 
 const Profit = () => {
   return (
-    <Container>
-      <Header title='جذب سود ساعت شمار' />
+    <ThinModal position='right'>
+      <Header title="جذب سود ساعت شمار" />
       <Tabs />
       <ProfitView />
-    </Container>
+    </ThinModal>
   );
 };
 
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,28 +1,18 @@
 import Header from "../components/Header";
 import SearchResults from "../components/search/SearchResults";
 import Tabs from "../components/search/Tabs";
-import styled from "styled-components";
+import ThinModal from "../components/ThinModal";
 import { useState } from "react";
-
-const Container = styled.div`
-background-color: #000000;
-  width: 450px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  padding: 15px 20px;
-`;
 
 const Search = () => {
   const [active, setActive] = useState("citizen");
 
   return (
-    <Container>
+    <ThinModal position='left'>
       <Header title="جستجوی مرکزی" />
       <Tabs active={active} setActive={setActive} />
       <SearchResults active={active} />
-    </Container>
+    </ThinModal>
   );
 };
 
